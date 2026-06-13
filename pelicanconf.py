43249c7ce5b79d06aa31e8a821738b44e7a66b16
Morgan Prior <36314+morganp@users.noreply.github.com>
2026-06-13 10:52:06 +0100

    Add AMBA Explorer as git submodule in Tools dropdown
    Adds morganp/amba-explorer as a submodule at content/amba-explorer,
    registers it as a static path, and wires it into the WEBAPPS nav menu.

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,7 +31,7 @@ READERS = {'html': None}
 # inside the directory content/
 # The contents of img/ will be available at 
 # {{ SITEURL }}/img
-STATIC_PATHS = ['images', 'pdf', 'extra', 'drum_rudiments', 'wavedrom-editor', 'fretdrom-editor']
+STATIC_PATHS = ['images', 'pdf', 'extra', 'drum_rudiments', 'wavedrom-editor', 'fretdrom-editor', 'amba-explorer']
 EXTRA_PATH_METADATA = {'extra/custom.css': {'path': 'static/custom.css'}}
 STYLESHEET_URL = '/static/custom.css'
 
@@ -94,4 +94,5 @@ WEBAPPS = [
     ('Drum Rudiments', '/drum_rudiments/'),
     ('Wavedrom Editor', '/wavedrom-editor/'),
     ('Fretdrom Editor', '/fretdrom-editor/'),
+    ('AMBA Explorer', '/amba-explorer/'),
 ]
